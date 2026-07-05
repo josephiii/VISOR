93b309dca5839cbe0daca1185e5ddacf10253553
Joseph Ales <162182688+josephiii@users.noreply.github.com>
2026-07-04 20:27:40 -0400

    refactor auth service logic to supabase req

--- backend/services/AuthService.py
+++ backend/services/AuthService.py
@@ -1,47 +1,22 @@
 
-import bcrypt
-from schemas.AuthSchema import RegisterSchema, LoginSchema
+from Database import supabase_client
 
 class AuthService:
 
-    def register(self, userData: RegisterSchema):
+    def register(self, username: str, email: str, password: str):
         
-        #TODO check db for existing user
-        userExists = None # <-- this should be a lookup that searches for user, if found returns true and user cannot register
-        if userExists:
-            return "User already exists change email/username"
+        database = supabase_client()
+        return database.auth.sign_up({
+            "email": email,
+            "password": password,
+            "options": {"data": {"username": username}}
+        })
+
+
+    def login(self, email: str, password: str):
         
-        #TODO add pwd requirements here
-
-        hashedPwd = bcrypt.hashpw(
-            userData.password.encode("utf-8"),
-            bcrypt.gensalt()
-        )
-
-        newUser = {
-            "username": userData.username,
-            "email": userData.email,
-            "password": hashedPwd.decode("utf-8")
-        }
-
-        #TODO add newUser to db
-
-        return "User Added" # <-- improve
-
-
-    def login(self, userData: LoginSchema):
-        
-        user = None # <-- this should be a look up that pulls the users info from db
-
-        if not user:
-            return "Username or pwd incorrect"
-        
-        validPwd = bcrypt.checkpw(
-            userData.password.encode("utf-8"),
-            user["password"].encode("utf-8")
-        )
-        
-        if not validPwd:
-            return "Username or pwd incorrect"
-        
-        #TODO return user object with jwt
+        database = supabase_client()
+        return database.auth.sign_in_with_password({
+            "email": email,
+            "password": password
+        })
