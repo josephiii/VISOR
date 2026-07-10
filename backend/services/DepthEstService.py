d63c65a0de606ccdea663ef39730a78c05b2087c
“Jaurelus” <hrqrdbrdzf@privaterelay.appleid.com>
2026-07-09 20:39:31 -0400

    run model + customizations

--- backend/services/DepthEstService.py
+++ backend/services/DepthEstService.py
@@ -1,31 +1,49 @@
 import torch
-from transformers import AutoImageProcessor, AutoModelForDepthEstimation, pipe
+from transformers import AutoImageProcessor, AutoModelForDepthEstimation
 from PIL import Image
 
 class Depth:
-    def compressImage():
+    def chooseDevice(self):
+        """
+        Determine the device type to run the model in either dev/production
+        """
+        if torch.cuda.is_available():
+            return "cuda"
+        elif torch.backends.mps.is_available():
+            return "mps"
+        else: return "cpu"
+
+    def __init__(self):
+        """
+        Load the models once to be used in the other functions
+        """
+        self.device = self.chooseDevice()
+        if self.device == "cuda":
+            self.dtype = torch.float16
+        else:
+            self.dtype = torch.float32
+
+        self.model = AutoModelForDepthEstimation.from_pretrained("depth-anything/Depth-Anything-V2-Metric-Indoor-Small-hf", torch_dtype=self.dtype ).to(self.device).eval()
+        self.image_processor = AutoImageProcessor.from_pretrained("depth-anything/Depth-Anything-V2-Metric-Indoor-Small-hf")
+    
+    
+    def compressImage(self):
         try:
             x=0
         except Exception as error:
             print(error)
         return x
-    def runModel(image):
+    def runModel(self ,image):
         """
         This functions runs the pre trained model. For the EC2 instance items from model should be on Cuda to use the GPU 
         """
-        try:
-            x=0
-            #Load and create model instance
-            model = AutoModelForDepthEstimation.from_pretrained("depth-anything/Depth-Anything-V2-Metric-Indoor-Small-hf", device_map="auto", torch_dtype=torch.float16 ).eval()
-            #Load image (call compressImage())
-            image_processor = AutoImageProcessor.from_pretrained("depth-anything/Depth-Anything-V2-Metric-Indoor-Small-hf")
-
+        try:            
             #Run only the inference part of the model
-            inputs= image_processor(images=image, return_tensors="pt").to( device=model.device, dtype=torch.float16)
+            inputs= self.image_processor(images=image, return_tensors="pt").to(self.device, self.dtype )
             with torch.no_grad():   
-                outputs= model(**inputs)
+                outputs= self.model(**inputs)
             
-            result = image_processor.post_process_depth_estimation(
+            result = self.image_processor.post_process_depth_estimation(
             outputs,
             target_sizes=[(image.height, image.width)],
             )
@@ -33,5 +51,4 @@ class Depth:
             
         except Exception as error:
             print(error)
-        return x
-    
+            return error
